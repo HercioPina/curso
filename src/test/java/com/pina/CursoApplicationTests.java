42f0cdeddb62bc26fb5562ad88bd9d2ebfa4b20c
Hercio Pina <hhherciooo@gmail.com>
2021-12-13 09:36:29 -0300

    Projeto criado

---
+++ src/test/java/com/pina/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
